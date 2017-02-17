d9c421c2b347f365069cc1d3638ba6cee60a6b06
ViniciusRCDS <vinicius.ricardo.2@gmail.com>
2017-02-16 21:16:37 -0300

    update

---
+++ src/br/unipe/java/unidade1/UserCanil.java
@@ -0,0 +1,17 @@
+package br.unipe.java.unidade1;
+/**
+ * 
+ * @author Vinicius Ricardo
+ *
+ */
+
+public class UserCanil {
+
+	public static void main(String[] args) {
+		Canil masmorra = new Canil ();
+        masmorra.alojar();
+       
+        masmorra.exterminaCachorro("Snow");
+	}
+
+}
