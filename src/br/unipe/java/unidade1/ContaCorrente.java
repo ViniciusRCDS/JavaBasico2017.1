de1a3854664f5ddf285056ad33638cc7ca6acb7f
ViniciusRCDS <vinicius.ricardo.2@gmail.com>
2017-03-01 21:50:41 -0300

    Revert "Revert "Update""
    This reverts commit fd4cf6226cd80c2af6b4e877c6545af3973a3abd.

---
+++ src/br/unipe/java/unidade1/ContaCorrente.java
@@ -0,0 +1,48 @@
+package br.unipe.java.unidade1;
+
+/**
+ * 
+ * @author Vinicius Ricardo
+ *
+ */
+
+public class ContaCorrente {
+
+	private int numeroConta;
+	private int numeroAgencia;
+	private float saldo;
+	private String cliente;
+
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+
+	public void setNumeroConta(int numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	public int getNumeroAgencia() {
+		return numeroAgencia;
+	}
+
+	public void setNumeroAgencia(int numeroAgencia) {
+		this.numeroAgencia = numeroAgencia;
+	}
+
+	public float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+
+	public String getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+
+}
